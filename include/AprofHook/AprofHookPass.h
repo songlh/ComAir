e9bebc045f780d820d815c9000044b6df485c988
tutengfei <tutengfei.kevin@gmail.com>
2018-01-12 21:19:51 -0500

    refs #2, finish insert aprof_init function into target bc's main function

---
+++ include/AprofHook/AprofHookPass.h
@@ -0,0 +1,51 @@
+#ifndef COMAIR_APROFHOOKPASS_H
+#define COMAIR_APROFHOOKPASS_H
+
+#include "llvm/Pass.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/GlobalVariable.h"
+
+
+using namespace llvm;
+
+struct AprofHook : public ModulePass
+{
+    static char ID;
+    AprofHook();
+
+
+
+    virtual void getAnalysisUsage(AnalysisUsage &AU) const;
+    virtual bool runOnModule(Module& M);
+
+    void SetupInit(Module *);
+    void SetupTypes(Module *);
+    void SetupConstants(Module *);
+    void SetupGlobals(Module *);
+    void SetupHooks(Module *);
+
+    void InsertAprofInit(Module *, Instruction *);
+
+    //type
+    IntegerType * IntType;
+    IntegerType * LongType;
+    Type * VoidType;
+
+    /* function */
+
+    // int aprof_init()
+    Function * aprof_init;
+
+    //global
+    GlobalVariable * aprof_count;
+    GlobalVariable * aprof_bb_count;
+
+    //constant
+    ConstantInt * ConstantLong0;
+    ConstantInt * ConstantLong1;
+
+};
+
+
+#endif //COMAIR_APROFHOOKPASS_H
